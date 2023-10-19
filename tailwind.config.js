082d3fc9e704335ce4f2fff51b193f5bec20f1bd
sakthi-sakthi <sakthiganapathis97@gmail.com>
2023-10-19 16:36:49 +0530

    update packages
    update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const tailwindcss = require("tailwindcss");
 
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: "media", // or 'media' or 'class'
+  darkMode: "media", 
   theme: {
     extend: {
       fontFamily: {
@@ -18,22 +18,3 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/forms")],
 };
-
-// module.exports = {
-//   purge: ['./src/**/*.{js,jsx,ts,tsx}','./public/index.html'],
-//   darkMode: false, // or 'media' or 'class'
-//   theme: {
-
-//     screens: {
-//       'tablet': '640px',
-//       // => @media (min-width: 640px) { ... }
-
-//       'laptop': '1024px',
-//       // => @media (min-width: 1024px) { ... }
-
-//       'desktop': '1280px',
-//       // => @media (min-width: 1280px) { ... }
-//     },
-//     plugins: [require('@tailwindcss/forms')],
-//   }
-// }
